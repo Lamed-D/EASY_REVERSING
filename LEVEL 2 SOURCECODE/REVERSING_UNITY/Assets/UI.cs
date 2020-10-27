6e12f1bb723e8af88ce6c177557155de22336e45
Dhhd <53391835+pec2016@users.noreply.github.com>
2020-10-27 23:23:52 +0900

    create

---
+++ LEVEL 2 SOURCECODE/REVERSING_UNITY/Assets/UI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    public int testvalue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (testvalue != 77777)
+        {
+            this.GetComponent<Text>().text = "TEST = " + testvalue.ToString("D5") + "\nTEST 값을 77777으로 바꾸면 FLAG가나옵니다 OR 시작하면 삭제될FLAG를살리세요";
+        }
+        else
+            this.GetComponent<Text>().text = "FLAG:{YOU_CRACKED_GAME}";
+    }
+}
---
+++ LEVEL 2 SOURCECODE/REVERSING_UNITY/Assets/spin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class spin : MonoBehaviour
+{
+    float spinSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        this.spinSpeed = 97;
+        transform.Rotate(0, 0, this.spinSpeed);
+
+        this.spinSpeed *= 0.96f;
+    }
+}
